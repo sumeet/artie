63d37df7757cf19db7982e3c827c286a66cf096f
Sumeet Agarwal <sumeet.a@gmail.com>
2010-08-03 07:55:24 -0400

    Add tests for joining channels.

--- test/tests.py
+++ test/tests.py
@@ -14,10 +14,19 @@ class TestServer(irc.IRC):
 		if not self.username or not self.realname:
 			self.username, _, _, self.realname = params
 			self._set_nickuserhost()
+			self.sendMessage('001')
 	
 	def irc_NICK(self, prefix, params):
 		self.nick = params[0]
 		self._set_nickuserhost()
+	
+	def irc_JOIN(self, prefix, params):
+		channel = params[0]
+		if channel not in self.factory.channels:
+			self.factory.channels[channel] = set()
+		self.factory.channels[channel].add(self)
+		if len(self.factory.channels) == 2:
+			self.factory.on_test_ready.callback(self)
 
 	def _set_nickuserhost(self):
 		if self.nick and self.username and self.hostname:
@@ -28,28 +37,28 @@ class TestServer(irc.IRC):
 				self.factory.users.remove(self.nickuserhost)
 			self.nickuserhost = nickuserhost
 			self.factory.users.add(nickuserhost)
-			self.factory.onConnectionMade.callback(self)
 
 	def connectionLost(self, *args):
-		self.factory.onConnectionLost.callback(self)
+		self.factory.on_connection_lost.callback(self)
 
 class BaseTest(unittest.TestCase):
 	def setUp(self):
 		self.server = Factory()
 		self.server.protocol = TestServer
 		self.server.users = set()
-		self.server_connected = defer.Deferred()
+		self.server.channels = {}
+		self.test_ready = defer.Deferred()
 		self.server_disconnected = defer.Deferred()
-		self.server.onConnectionMade = self.server_connected
-		self.server.onConnectionLost = self.server_disconnected
+		self.server.on_test_ready = self.test_ready
+		self.server.on_connection_lost = self.server_disconnected
 		self.server_port = reactor.listenTCP(7890, self.server)
 
-		artie = self.artie_factory()
+		self.artie = self.artie_factory()
 		self.client_connection = reactor.connectTCP(
-			settings.SERVER, 7890, artie
+			settings.SERVER, 7890, self.artie
 		)
 
-		return self.server_connected
+		return self.test_ready
 
 	def tearDown(self):
 		self.client_connection.disconnect()
@@ -72,5 +81,11 @@ class TestConnection(BaseTest):
 		self.assertEqual(nick, 'nick')
 		self.assertEqual(user, 'user')
 
+	def test_client_joined_channels(self):
+		self.assertEqual(len(self.server.channels), 2)
+		self.assertTrue('#channel1' in self.server.channels)
+		self.assertTrue('#channel2' in self.server.channels)
+
 if __name__ == '__main__':
-	print 'Run with `trial tests`'
+	reactor.listenTCP(7890, _create_server_factory())
+	reactor.run()
